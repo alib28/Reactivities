ba3ccd66a77662400465f073d6fae674b1b9f396
Ali Baghdadi <ali.baghdadi@gotocme.com>
2020-04-08 09:09:26 +0300

    End of Section 7

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,24 +1,15 @@
-import React, { SyntheticEvent } from 'react'
-import { Item, Button, Label, Segment } from 'semantic-ui-react'
-import { IActivity } from '../../../app/models/activity'
+import React, { useContext } from 'react'
+import { observer } from 'mobx-react-lite';
+import { Item, Button, Label, Segment } from 'semantic-ui-react';
+import ActivityStore from '../../../app/stores/activityStore';
 
-interface IProps {
-    activities: IActivity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
-    submitting: boolean;
-    target: string
-}
-
-export const ActivityList: React.FC<IProps> = ({ activities,
-    selectActivity,
-    deleteActivity,
-    submitting,
-    target }) => {
+const ActivityList: React.FC = () => {
+    const activityStore = useContext(ActivityStore);
+    const {activitiesByDate, selectActivity, deleteActivity, target, submitting } = activityStore;
     return (
         <Segment clearing>
             <Item.Group divided>
-                {activities.map(activity => (
+                {activitiesByDate.map(activity => (
                     <Item key={activity.id}>
                         <Item.Content>
                             <Item.Header as='a'>{activity.title}</Item.Header>
@@ -48,5 +39,7 @@ export const ActivityList: React.FC<IProps> = ({ activities,
 
             </Item.Group>
         </Segment>
-    )
+    );
 }
+
+export default observer(ActivityList);
